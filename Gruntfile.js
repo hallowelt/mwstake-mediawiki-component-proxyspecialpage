2f82a4bde2cb1eff4e2761c35a630d2043beda46
Hamish <hamish.slater@hallowelt.com>
2026-07-21 01:39:10 +0700

    banana

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		banana: {
+			all: 'i18n/'
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
