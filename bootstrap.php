d95aec4cbe5b276e96f79a172fc37f3d44f4d159
d.savuljesku <d.savuljesku@gmail.com>
2026-07-20 10:04:31 +0200

    Fix message key

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_PROXYSPECIALPAGE_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_PROXYSPECIALPAGE_VERSION', '1.0.0' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_PROXYSPECIALPAGE_VERSION', '1.0.1' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'proxyspecialpage', static function () {
