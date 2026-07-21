8b78c82b87fedee51669ed3a89bf459f11ca947b
Hamish <hamish.slater@hallowelt.com>
2026-07-21 22:13:36 +0700

    Housekeeping
    ERM49171

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,21 @@
 
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 
 	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true,
+				fix: grunt.option( 'fix' )
+			},
+			all: '.'
+		},
 		banana: {
 			all: 'i18n/'
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
